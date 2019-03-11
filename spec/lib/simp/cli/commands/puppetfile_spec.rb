ea18c8a1af86d0c5addb2f62006baa0f068ca63a
Chris Tessmer <chris.tessmer@onyxpoint.com>
2019-03-11 22:05:15 +0000

    Add test

---
+++ spec/lib/simp/cli/commands/puppetfile_spec.rb
@@ -0,0 +1,12 @@
+require 'simp/cli/commands/puppetfile'
+require 'spec_helper'
+
+describe Simp::Cli::Commands::Puppetfile do
+  describe '#foo' do
+    puts 'WAT'
+    it 'foo' do
+require 'pry'; binding.pry
+       subject.run
+    end
+  end
+end
