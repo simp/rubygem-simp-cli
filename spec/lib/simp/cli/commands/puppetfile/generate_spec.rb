2b10063bf33a2fec63cef644e5f80035a8ef0b35
Chris Tessmer <chris.tessmer@onyxpoint.com>
2019-03-14 20:27:01 +0000

    Add generate tests

--- spec/lib/simp/cli/commands/puppetfile/generate_spec.rb
+++ spec/lib/simp/cli/commands/puppetfile/generate_spec.rb
@@ -4,15 +4,22 @@ require 'spec_helper'
 describe Simp::Cli::Commands::Puppetfile::Generate do
   describe '#run' do
     context 'with default arguments' do
+      subject(:run) { proc { described_class.new.run([])} }
       it 'prints puppetfile to stdout' do
         allow(Simp::Cli::Puppetfile::LocalSimpPuppetModules).to receive(:new).and_return(
           object_double('Fake LocalSimpPuppetModules', :to_puppetfile => 'Mocked Puppetfile content')
         )
-        expect{ subject.run ['generate'] }.to output("Mocked Puppetfile content\n").to_stdout
+        expect{ run.call }.to output("Mocked Puppetfile content\n").to_stdout
       end
     end
     context 'with argument `--skeleton`' do
-      it 'prints puppetfile to stdout'
+      subject(:run_sk) { proc { described_class.new.run(['--skeleton'])} }
+      it 'prints puppetfile to stdout' do
+        allow(Simp::Cli::Puppetfile::Skeleton).to receive(:to_puppetfile).and_return(
+          'Mocked Puppetfile content'
+        )
+        expect{ run_sk.call }.to output("Mocked Puppetfile content\n").to_stdout
+      end
     end
   end
 end
