9d5153cb8a9dd0f995d09ce60c7a91501cd7c50b
lnemsick-simp <lnemsick.simp@gmail.com>
2021-02-08 10:16:20 -0500

    (SIMP-9378) simp-cli: allow empty domain list EL8
    - Fixed a bug where sssd::domains validation did not allow and
      empty domain list in EL8, even though it recommended an empty
      domain list.
    - Removed EL6-specific test logic that no longer applies, as
      simp-rsync-skeleton no longer installs those directories.
    
    SIMP-9378 #close

--- spec/lib/simp/cli/config/items/data/sssd_domains_spec.rb
+++ spec/lib/simp/cli/config/items/data/sssd_domains_spec.rb
@@ -3,12 +3,11 @@ require 'rspec/its'
 require_relative '../spec_helper'
 
 describe Simp::Cli::Config::Item::SssdDomains do
-  before :each do
-    @ci = Simp::Cli::Config::Item::SssdDomains.new
-    @ci.silent = true
-  end
-
   describe '#recommended_value' do
+    before :each do
+      @ci = Simp::Cli::Config::Item::SssdDomains.new
+      @ci.silent = true
+    end
 
     context "when 'simp_options::ldap' is 'true'" do
       it "should return ['LDAP']" do
@@ -21,31 +20,79 @@ describe Simp::Cli::Config::Item::SssdDomains do
 
     context "when 'simp_options::ldap' is 'false'" do
       context 'OS major version < 8' do
-        it "should return ['LOCAL']" do
+        before :each do
           os_fact = { 'release' => { 'major' => '7' } }
           allow(Facter).to receive(:value).with('os').and_return(os_fact)
 
           item = Simp::Cli::Config::Item::SimpOptionsLdap.new
           item.value = false
           @ci.config_items[item.key] = item
+        end
+
+        it "should return ['LOCAL']" do
           expect( @ci.recommended_value ).to eq ['LOCAL']
         end
       end
 
       context 'OS major version >= 8' do
-        it "should return []" do
+        before :each do
           os_fact = { 'release' => { 'major' => '8' } }
           allow(Facter).to receive(:value).with('os').and_return(os_fact)
 
           item = Simp::Cli::Config::Item::SimpOptionsLdap.new
           item.value = false
           @ci.config_items[item.key] = item
+        end
+
+        it 'should return []' do
           expect( @ci.recommended_value ).to eq []
         end
       end
     end
-
   end
 
-  it_behaves_like "a child of Simp::Cli::Config::Item"
+  describe '#validate' do
+    context 'OS major version < 8' do
+      before :each do
+        os_fact = { 'release' => { 'major' => '7' } }
+        allow(Facter).to receive(:value).with('os').and_return(os_fact)
+
+        @ci = Simp::Cli::Config::Item::SssdDomains.new
+        @ci.silent = true
+        item = Simp::Cli::Config::Item::SimpOptionsLdap.new
+        item.value = false
+        @ci.config_items[item.key] = item
+      end
+
+      it 'should reject an empty domain list' do
+        expect( @ci.validate([]) ).to eq false
+      end
+    end
+
+    context 'OS major version >= 8' do
+      before :each do
+        os_fact = { 'release' => { 'major' => '8' } }
+        allow(Facter).to receive(:value).with('os').and_return(os_fact)
+
+        @ci = Simp::Cli::Config::Item::SssdDomains.new
+        @ci.silent = true
+        item = Simp::Cli::Config::Item::SimpOptionsLdap.new
+        item.value = false
+        @ci.config_items[item.key] = item
+      end
+
+      it 'should accept an empty domain list' do
+        expect( @ci.validate([]) ).to eq true
+      end
+    end
+  end
+
+  context 'base operation' do
+    before :each do
+      @ci = Simp::Cli::Config::Item::SssdDomains.new
+      @ci.silent = true
+    end
+
+    it_behaves_like "a child of Simp::Cli::Config::Item"
+  end
 end
